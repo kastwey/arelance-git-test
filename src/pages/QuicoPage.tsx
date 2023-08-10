034c1462636c1a1cf100856274f605d931ed1099
Quico Saval <quicosv@gmail.com>
2023-08-10 18:43:54 +0200

    Crea la página de Quico

---
+++ src/pages/QuicoPage.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../components/layout";
+
+export function QuicoPage() {
+  return (
+    <Layout>
+      <h2>Quico</h2>
+	  <p>Quico crea una página como otra culaquiera.</p>
+    </Layout>
+  );
+}
