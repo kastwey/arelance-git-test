1272f6d67be89d901de83bfcde0c3003a7ce577c
Ken Masters <mskenmasters@hotmail.com>
2023-08-10 18:45:41 +0200

    He añadido mi página personal

---
+++ src/pages/BilalPage .tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/layout";
+
+export function BilalPage() {
+  return (
+    <Layout>
+      <h2>Bilal</h2>
+      <p>¡Este es Bilal!</p>
+      <p>Dirección de correo electrónico: bilal@porlacara.com</p>
+    </Layout>
+  );
+}
