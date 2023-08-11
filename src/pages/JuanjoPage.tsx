ed363072ddc78232b274eb1f020256906f4482c6
Juanjo M <jmontiel@microsoft.com>
2023-08-11 16:51:26 +0200

    borro encabezado

--- src/pages/JuanjoPage.tsx
+++ src/pages/JuanjoPage.tsx
@@ -3,7 +3,6 @@ import { Layout } from "../components/layout";
 export function JuanjoPage() {
   return (
     <Layout>
-      <h2>Juanjo</h2>
       <p>¡Este es juanjo!</p>
       <p>Añado un texto aquí.</p>
       <p>¿Habéis visto qué feo es?</p>
