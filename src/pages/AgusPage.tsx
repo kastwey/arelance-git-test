50a173c630f7bb15d14f19219f1fa4a481207e16
Agustín Aguirre Rabino <aguirre.mdp@gmail.com>
2023-08-11 18:42:01 +0200

    Cambio el encabezado con mi n ombre

--- src/pages/AgusPage.tsx
+++ src/pages/AgusPage.tsx
@@ -3,7 +3,7 @@ import { Layout } from "../components/layout";
 export function AgusPage() {
   return (
     <Layout>
-      <h2>Agus</h2>
+      <h2>Agustín Aguirre </h2>
       <p>Soy un párrafo y se supone que cuento cosas de Agus que son totalmente irrelevantes</p>
     </Layout>
   );
